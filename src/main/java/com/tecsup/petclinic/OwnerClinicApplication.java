d6f0f8f9f25851242c36c803dda86f9eb7bcf7b6
Jeremy <jeremi.nunez@tecsup.edu.pe>
2019-05-23 23:41:22 -0500

    Ev03

---
+++ src/main/java/com/tecsup/petclinic/OwnerClinicApplication.java
@@ -0,0 +1,10 @@
+package com.tecsup.petclinic;
+
+import org.springframework.boot.SpringApplication;
+
+public class OwnerClinicApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerClinicApplication.class, args);
+	}
+}
